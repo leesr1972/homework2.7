85a40f8daf01b4c126bf43fff5274feb3bbc1958
Ли Сергей <sergey.r.lee@gmail.com>
2022-04-09 13:28:10 +0300

    Комит к ДЗ № 2.7.

---
+++ src/test/java/pro/sky/java/course2/homework2_7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
